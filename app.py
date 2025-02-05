5c1d159d0a5fbbb496011054d7af25558ca50ea1
Hanif <hanif@bmuse.com>
2025-02-05 18:20:15 +0530

    check for cuda

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+import torch
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!!"}
+    if torch.cuda.is_available():
+        version = torch.version.cuda
+        gpu_name = torch.cuda.get_device_name(0)
+        no_of_gpus = torch.cuda.device_count()
+        return {"message": f"CUDA Available!! {version}, {gpu_name}, {no_of_gpus}"}
+    
+    return {"message": "CUDA not available"}
